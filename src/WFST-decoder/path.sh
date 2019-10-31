3611e1a8c4ea17ad7fd6d49dd913be76ad302a72
kkulczak <kulczakkacper@gmail.com>
2019-10-31 10:32:59 +0100

    kaldi path fixes

--- src/WFST-decoder/path.sh
+++ src/WFST-decoder/path.sh
@@ -1,6 +1,6 @@
 export KALDI_ROOT=/opt/kaldi/
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
-export SRILM=/opt/kaldi/kaldi/tools/srilm
+export SRILM=/opt/kaldi/tools/srilm
 export PATH=${PATH}:${SRILM}/bin:${SRILM}/bin/i686-m64
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${KALDI_ROOT}/src/lib
